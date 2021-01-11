52f47b6850f927286a8d44acc4ce33586025374c
tsokac2 <tsokac2@gmail.com>
2021-01-11 12:46:34 +0000

    Implement basic view and routing

--- run.py
+++ run.py
@@ -6,10 +6,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello There</h1>"
+    """ Main page with instructions """
+    return "To send a message use/USERNAME/MESSAGE"
 
 
-if __name__ =="__main__":
-    app.run(host=os.getenv("IP"),
-       port=int(os.getenv("PORT")),
-       debug=True)
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
