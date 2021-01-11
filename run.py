2130d743d7731b831b8664606469fde696a93423
tsokac2 <tsokac2@gmail.com>
2021-01-11 18:17:42 +0000

    Final commit

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, render_template, request, session, url_for
 
 
 app = Flask(__name__)
-app.secret_key = "randomstring123"
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
 
@@ -38,9 +38,6 @@ def user(username):
     return render_template("chat.html", username=username,
                            chat_messages=messages)
 
-if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True
-    )
+
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=False)
